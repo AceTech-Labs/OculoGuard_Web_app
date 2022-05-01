0836ca945b3090f44e069bd18a9c0de7fabdd5c3
Thidas Jayawardane <thidas.2019717@iit.ac.lk>
2022-05-01 19:11:39 +0530

    index jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,9 @@
 import { useState, useEffect } from "react";
 import Head from "next/head";
-import Layout from "../components/layout/Layout";
 import { API, Auth } from "aws-amplify";
 import { listUsers, getOphthalmologist } from "../src/graphql/queries";
-import { createOphthalmologist } from "../src/graphql/mutations";
 import Button from "../components/buttons/Button";
+import Link from "next/link";
 
 const defaultAuthMode = "AMAZON_COGNITO_USER_POOLS";
 
@@ -14,83 +13,76 @@ export default function Home() {
 
   useEffect(() => {
     checkOphthalamologist();
+    getAllUsers();
   }, []);
 
   const signOut = async () => {
     await Auth.signOut();
   };
 
-  const checkOphthalamologist = async (id) => {
-    const userInfo = await Auth.currentUserInfo();
-    console.log(userInfo);
-    const results = await API.graphql({
-      query: getOphthalmologist,
-      variables: {
-        id: userInfo.id,
-      },
-      authMode: defaultAuthMode,
-    });
-    if (results.data.getOphthalmologist === null) {
-      setOphthalamologist("New Optha");
-    } else {
-      setOphthalamologist(results.data.getOphthalmologist.name);
+  const checkOphthalamologist = async () => {
+    const userInfo = await Auth.currentAuthenticatedUser();
+    try {
+      const { data } = await API.graphql({
+        query: getOphthalmologist,
+        variables: {
+          id: userInfo.attributes.sub,
+        },
+        authMode: defaultAuthMode,
+      });
+      if (data.getOphthalmologist === null) {
+        setOphthalamologist("NEW");
+      } else {
+        setOphthalamologist(data.getOphthalmologist.name);
+      }
+    } catch (e) {
+      console.log(e);
     }
   };
 
-  const addOphthalamologist = async () => {
-    const userInfo = await Auth.currentUserInfo();
-    await API.graphql({
-      query: createOphthalmologist,
-      variables: {
-        input: {
-          id: userInfo.id,
-          name: "Demo_Account_Optho_",
-        },
-      },
-      authMode: defaultAuthMode,
-    });
-  };
-
   const getAllUsers = async () => {
-    const results = await API.graphql({
+    const { data } = await API.graphql({
       query: listUsers,
       authMode: defaultAuthMode,
     });
-    console.log(results);
-    // setUsers([...results]);
+    console.log(data);
+    setUsers([...data.listUsers.items]);
   };
 
   return (
-    <Layout>
-      <div className="bg-red flex flex-col">
-        <Head>OculoGuard</Head>
-        <main>say my name</main>
-        <div>
-          <button onClick={signOut}>Sign Out</button>
-        </div>
-        <main>
-          <p className="bg-red text-center">
-            User name {ophthalamologist == null ? "null" : ophthalamologist}
-          </p>
-          <div>
-            {users.map((user) => {
-              return <div key={user.id}>{user.name}</div>;
-            })}
-          </div>
-          <div className="flex justify-center py-7">
-            say my name
-            <div>
-              <Button onClick={addOphthalamologist}>
-                Add Ophthalamologist
-              </Button>
-            </div>
-            <div>
-              <Button onClick={getAllUsers}>List Users</Button>
-            </div>
-          </div>
-          <div></div>
-        </main>
+    <div className="bg-red flex flex-col">
+      <Head>OculoGuard</Head>
+      <main>say my name</main>
+
+      <div>
+        <button onClick={signOut}>Sign Out</button>
       </div>
-    </Layout>
+      <main>
+        <p className="bg-red text-center">
+          User name{" "}
+          {ophthalamologist === "NEW" ? (
+            <span>
+              Seems like you are new to the System please create account from{" "}
+              <Link href={"/profile"}>here</Link>
+            </span>
+          ) : (
+            ophthalamologist
+          )}
+        </p>
+        <div>
+          {users.map((user) => {
+            return <div key={user.id}>{user.name}</div>;
+          })}
+        </div>
+        <div className="flex justify-center py-7">
+          say my name
+          <div></div>
+          <div>
+            <Button onClick={getAllUsers}>List Users</Button>
+          </div>
+        </div>
+        <div></div>
+      </main>
+    </div>
   );
 }
