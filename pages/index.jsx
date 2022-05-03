2ceab83878a1d6e062d9de3a8993818fbcaa4512
Thidas Jayawardane <thidas.2019717@iit.ac.lk>
2022-05-03 09:46:54 +0530

    updated compatible with both Doctors and Patience

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,27 +1,36 @@
 import { useState, useEffect } from "react";
 import Head from "next/head";
 import { API, Auth } from "aws-amplify";
-import { listUsers, getOphthalmologist } from "../src/graphql/queries";
+import { listUsers, getOphthalmologist, getUser } from "../src/graphql/queries";
 import Button from "../components/buttons/Button";
 import Link from "next/link";
+import Card from "../components/card/Card";
+import { data } from "autoprefixer";
+import HeadDisplay from "../components/HeadDisplay";
+import { useRouter } from "next/router";
 
 const defaultAuthMode = "AMAZON_COGNITO_USER_POOLS";
+let predictor = "/predictor/";
 
 export default function Home() {
-  const [ophthalamologist, setOphthalamologist] = useState(null);
+  const [userName, setUserName] = useState(null);
   const [users, setUsers] = useState([]);
+  const [assignedUsers, setAssignedUsers] = useState([]);
+  const [user, setUser] = useState(null);
+  const [userId, setUserId] = useState(null);
+  const router = useRouter();
 
   useEffect(() => {
     checkOphthalamologist();
     getAllUsers();
+    getAssignedUsers();
   }, []);
 
-  const signOut = async () => {
-    await Auth.signOut();
-  };
+  // useEffect(() => {}, [user]);
 
   const checkOphthalamologist = async () => {
     const userInfo = await Auth.currentAuthenticatedUser();
+    console.log(userInfo.attributes.sub);
     try {
       const { data } = await API.graphql({
         query: getOphthalmologist,
@@ -30,10 +39,37 @@ export default function Home() {
         },
         authMode: defaultAuthMode,
       });
+
       if (data.getOphthalmologist === null) {
-        setOphthalamologist("NEW");
+        setUser("P");
       } else {
-        setOphthalamologist(data.getOphthalmologist.name);
+        setUser("O");
+        setUserName(data.getOphthalmologist.name);
+        if (data.getOphthalmologist.name === "") {
+          setUserName("NEW");
+        } else {
+          setUserName(data.getOphthalmologist.name);
+        }
+        return;
+      }
+    } catch (e) {
+      console.log(e);
+    }
+
+    try {
+      const { data } = await API.graphql({
+        query: getUser,
+        variables: {
+          id: userInfo.attributes.sub,
+        },
+        authMode: defaultAuthMode,
+      });
+      console.log("data = ", data);
+      if (data.getUser === null) {
+        setUserName("NEW");
+      } else {
+        setUserName(data.getUser.name);
+        setUserId(userInfo.attributes.sub);
       }
     } catch (e) {
       console.log(e);
@@ -41,47 +77,124 @@ export default function Home() {
   };
 
   const getAllUsers = async () => {
-    const { data } = await API.graphql({
-      query: listUsers,
-      authMode: defaultAuthMode,
-    });
-    console.log(data);
-    setUsers([...data.listUsers.items]);
+    try {
+      const { data } = await API.graphql({
+        query: listUsers,
+        authMode: defaultAuthMode,
+      });
+      setUsers([
+        ...data.listUsers.items.filter((user) => {
+          return user.ophthalmologistID === null;
+        }),
+      ]);
+    } catch (e) {
+      console.log(e);
+    }
   };
+
+  const getAssignedUsers = async () => {
+    const currentUser = await Auth.currentAuthenticatedUser();
+    try {
+      const { data } = await API.graphql({
+        query: listUsers,
+        authMode: defaultAuthMode,
+      });
+      setAssignedUsers([
+        ...data.listUsers.items.filter((user) => {
+          return user.ophthalmologistID === currentUser.attributes.sub;
+        }),
+      ]);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  if (user === "P") {
+    return (
+      <div>
+        <HeadDisplay>
+          Welcome back &nbsp;
+          {userName === "NEW" ? (
+            <span>
+              Seems like you are new to the System please create account from
+              <Link href={"/profile"}> here</Link>
+            </span>
+          ) : (
+            userName
+          )}
+          !
+        </HeadDisplay>
+        <div className="my-12"></div>
+        <div className="flex justify-center">
+          {userId && (
+            <Button
+              onClick={() => {
+                router.push(predictor + userId);
+              }}
+            >
+              Go For Prediction
+            </Button>
+          )}
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className="bg-red flex flex-col">
       <Head>OculoGuard</Head>
-      <main>say my name</main>
-
-      <div>
-        <button onClick={signOut}>Sign Out</button>
-      </div>
-      <main>
-        <p className="bg-red text-center">
-          User name{" "}
-          {ophthalamologist === "NEW" ? (
+      <main className="flex flex-col">
+        <HeadDisplay>
+          Welcome back &nbsp;
+          {userName === "NEW" ? (
             <span>
-              Seems like you are new to the System please create account from{" "}
-              <Link href={"/profile"}>here</Link>
+              Seems like you are new to the System please create account from
+              <Link href={"/profile"}> here</Link>
             </span>
           ) : (
-            ophthalamologist
+            userName
           )}
-        </p>
-        <div>
-          {users.map((user) => {
-            return <div key={user.id}>{user.name}</div>;
-          })}
-        </div>
-        <div className="flex justify-center py-7">
-          say my name
-          <div></div>
-          <div>
-            <Button onClick={getAllUsers}>List Users</Button>
+          !
+        </HeadDisplay>
+        <div className="my-12"></div>
+        <div className="flex items-baseline w-full">
+          <div className="">
+            <div className="flex justify-center my-4 text-red font-bold">
+              Your Users
+            </div>
+            {assignedUsers.map((user) => {
+              return (
+                <Card
+                  key={user.id}
+                  id={user.id}
+                  age={user.age}
+                  records={user.Records.items.length}
+                >
+                  {user.name}
+                </Card>
+              );
+            })}
+          </div>
+          <div className="flex mx-44"></div>
+          <div className="">
+            <div className="flex justify-center my-4 font-bold">
+              Non Assigned Users
+            </div>
+            {users.map((user) => {
+              return (
+                <Card
+                  key={user.id}
+                  id={user.id}
+                  age={user.age}
+                  state="UNASSIGNED"
+                  records={user.Records.items.length}
+                >
+                  {user.name}
+                </Card>
+              );
+            })}
           </div>
         </div>
-        <div></div>
       </main>
     </div>
   );
